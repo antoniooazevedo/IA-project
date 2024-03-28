682ed150e2f5718ac079beedf22af043a7c2da89
António Azevedo <antazevedo2003@gmail.com>
2024-03-28 18:49:36 +0000

    corrected level formats

--- MVC/Controller/Menus/level_menu_controller.py
+++ MVC/Controller/Menus/level_menu_controller.py
@@ -55,9 +55,9 @@ class Level_Menu_Controller:
         self.level_controller = Level_Controller(self.level_model)
         
         state = Sokobond_State(self.level_model)
-        goal = Search.greedy_search(state, Heuristic.prioritize_free_electrons)
+        goal = Search.a_star_search(state, Heuristic.manhattan_distance)
         self.moves = Search.get_solution_moves(goal)
-        #Search.print_solution(goal)
+        Search.print_solution(goal)
     
     def play_level(self):
         if len(self.moves) != 0:
