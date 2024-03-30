cc38c01960cc2c0239ee812c6359c25d0e423f5b
Tomas Martins <tomas.eiras03@gmail.com>
2024-03-30 11:19:36 +0000

    started

--- sokobond.py
+++ sokobond.py
@@ -2,7 +2,13 @@ import pygame as pg
 from MVC.Model.menu_model import Menu_Model
 from MVC.View.menu_view import Menu_View
 from MVC.Controller.Menus.main_menu_controller import Main_Menu_Controller
+from MVC.Model.level_model import Level_Model
+from MVC.View.level_view import Level_View
+from MVC.Controller.level_controller import Level_Controller
+from AI.sokobond_state import Sokobond_State
+from AI.tree_node import Search, Heuristic
 import utils as utils
+import time
 
 # Constants
 WIDTH, HEIGHT = 800, 600
@@ -11,10 +17,11 @@ WINDOW_SIZE = (WIDTH, HEIGHT)
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, ai_type, level_name):
         pg.init()
 
-        self.levelName = "lvl1.txt"
+        self.ai_type = ai_type
+        self.levelName = level_name
 
         pg.display.set_caption("Sokobond")
         self.screen = pg.display.set_mode(WINDOW_SIZE)
@@ -24,22 +31,67 @@ class Game:
 
     def run(self):
 
-        main_menu_model = Menu_Model(["Play", "Choose AI", "Quit"], "SOKOBOND", 50)
-        main_menu_view = Menu_View(self.screen, main_menu_model, 1)
-        main_menu_controller = Main_Menu_Controller(main_menu_model, self.screen)
+        #main_menu_model = Menu_Model(["Play", "Choose AI", "Quit"], "SOKOBOND", 50)
+        #main_menu_view = Menu_View(self.screen, main_menu_model, 1)
+        #main_menu_controller = Main_Menu_Controller(main_menu_model, self.screen)
 
-        while True:
-            main_menu_controller.handle_events()
+        self.level_model = Level_Model(self.levelName)
+        self.level_controller = Level_Controller(self.level_model) 
+        state = Sokobond_State(self.level_model)
+        start_time = time.time()
+        goal = self.create_AI(state)
+        end_time = time.time()
 
-            if (
-                (not main_menu_controller.on_level_menu)
-                and (not main_menu_controller.level_menu_controller.playing)
-                and (not main_menu_controller.on_ai_menu)
-            ):
-                main_menu_view.draw()
-                pg.display.update()
+        execution_time = end_time - start_time
 
-            self.clock.tick(self.fps)
+        return execution_time
+    
+        #while (not self.level_model.won):
+        #    
+        #    self.level_controller.handle_events()
+        #    
+        #    self.clock.tick(self.fps)
+        #    
+        #    self.level_controller.check_win()
+
+    def create_AI(self, state):
+        goal = None
+
+        if self.ai_type == "BFS":
+            goal = Search.breadth_first_search(state)
+        elif self.ai_type == "DFS":
+            goal = Search.depth_first_search(state)
+        elif self.ai_type == "Depth Limited":
+            goal = Search.depth_limited_search(state, 25)
+        elif self.ai_type == "Iterative Deepening":
+            goal = Search.iterative_deepening_search(state, 24)
+        elif self.ai_type == "Greedy - Manhattan Distance":
+            goal = Search.greedy_search(state, Heuristic.manhattan_distance)
+        elif self.ai_type == "Greedy - Free Electrons":
+            goal = Search.greedy_search(state, Heuristic.prioritize_free_electrons)
+        elif self.ai_type == "Greedy - Minimize Free Electrons":
+            goal = Search.greedy_search(state, Heuristic.minimize_free_electrons)
+        elif self.ai_type == "A* - Manhattan Distance":
+            goal = Search.a_star_search(state, Heuristic.manhattan_distance)
+        elif self.ai_type == "A* - Free Electrons":
+            goal = Search.a_star_search(state, Heuristic.prioritize_free_electrons)
+        elif self.ai_type == "A* - Minimize Free Electrons":
+            goal = Search.a_star_search(state, Heuristic.minimize_free_electrons)
+
+        if goal == None:
+            return
+
+        self.moves = Search.get_solution_moves(goal)     
+
+        return goal   
+    
+
+algorithms = ["A* - Manhattan Distance", "A* - Free Electrons"]
+levels = ["lvl1.txt", "lvl2.txt", "lvl3.txt", "lvl6.txt", "lvl7.txt", "lvl8.txt"]
 
 
-Game().run()
+for algorithm in algorithms:
+    for level in levels:
+        game = Game(algorithm, level)
+        execution_time = game.run()
+        print(f"Algorithm: {algorithm}, Level: {level}, Execution time: {execution_time}")
